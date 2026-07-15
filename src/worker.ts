c04027af556fecce173bcfca86ac56101166f815
cocmo1024 <cocmo1024@users.noreply.github.com>
2026-07-15 20:47:12 +0800

    fix: canonicalize sitemap and domain routing

--- src/worker.ts
+++ src/worker.ts
@@ -4,12 +4,28 @@ interface Env {
   };
 }
 
+const CANONICAL_HOST = 'titanium3dp.com';
+const PRODUCTION_HOSTS = new Set([CANONICAL_HOST, `www.${CANONICAL_HOST}`]);
+const LEGACY_SITEMAP_PATH = '/sitemap.xml';
+
 export default {
   async fetch(request: Request, env: Env): Promise<Response> {
     const url = new URL(request.url);
+    let shouldRedirect = false;
 
-    if (url.hostname === 'www.titanium3dp.com') {
-      url.hostname = 'titanium3dp.com';
+    if (PRODUCTION_HOSTS.has(url.hostname) && (url.protocol !== 'https:' || url.hostname !== CANONICAL_HOST)) {
+      url.protocol = 'https:';
+      url.hostname = CANONICAL_HOST;
+      url.port = '';
+      shouldRedirect = true;
+    }
+
+    if (url.pathname === LEGACY_SITEMAP_PATH) {
+      url.pathname = '/sitemap-index.xml';
+      shouldRedirect = true;
+    }
+
+    if (shouldRedirect) {
       return Response.redirect(url.toString(), 301);
     }
 
